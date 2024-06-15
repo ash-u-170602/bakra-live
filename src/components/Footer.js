88472b9a2fe9ae6e175bd0a8664b62c56994ef1a
Ashad Naushad <ashad.naushad@webmingo.in>
2024-06-15 10:21:54 +0530

    removed .

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -9,7 +9,7 @@ const Footer = () => {
         <img src={logo} alt="Logo" className="footer-logo" />
         <a href="mailto:contact@bakra.live" className="footer-email">contact@bakra.live</a>
         <p className="footer-catchy-line">Celebrate Eid-ul-Adha with the Best, Find Your Perfect Qurbani Goat Today!</p>
-        <p className="footer-text">© 2024 Bakra.live. All rights reserved.</p>
+        <p className="footer-text">© 2024 Bakra.live All rights reserved.</p>
       </div>
     </div>
   );
